885d84bf24f42bb4db2daccaf54083adf61255a6
Adam <adamgold@seattleacademy.org>
2015-05-29 08:52:44 -0700

    add fire4 and make movements smaller when using shift

--- turnerkeyboard.js
+++ turnerkeyboard.js
@@ -33,6 +33,12 @@ setTimeout(function(){ThunderConnector.command('left');},8000);
 setTimeout(function(){ThunderConnector.command('up');},11050);
 setTimeout(function(){ThunderConnector.command('stop');},11850);
 }
+function fire4(){
+  setTimeout(function(){ThunderConnector.command('fire');},0);
+  setTimeout(function(){ThunderConnector.command('fire');},3500);
+  setTimeout(function(){ThunderConnector.command('fire');},7000);
+  setTimeout(function(){ThunderConnector.command('fire');},10500);
+}
 
 // listen for the "keypress" event
 process.stdin.on('keypress', function (ch, key) {
@@ -51,32 +57,34 @@ process.stdin.on('keypress', function (ch, key) {
   if (key.name == "d") {
     turnRightDegrees(2.0);
   }
-  if (key.name == "up") {
+  if (key.name == "w" && key.shift == true) {
     turnUpDegrees(0.5);    
   }
-  if (key.name == "left") {
+  if (key.name == "a" && key.shift == true) {
     turnLeftDegrees(0.5);
   }
-  if (key.name == "down") {
+  if (key.name == "s" && key.shift == true) {
     turnDownDegrees(0.5);
   }
-  if (key.name == "right") {
+  if (key.name == "d" && key.shift == true) {
     turnRightDegrees(0.5);
   }
+
+// if shift is pressed while using W, A, S, D, the launcher will move .5 degrees instead of 2
+
   if (key.name == "c" && key.ctrl == false) {
     center();
   }
-
-  // if (key.name == "shift" && key.name == "w")
-    // {
-    //   turnUpDegrees(1);
-    // }
-
-  if (key.name == "f" || key.name == "space") {
-    // console.log("Fire");
+  if (key.name == "f") {
     setTimeout(function(){ThunderConnector.command('fire');},0);
     setTimeout(function(){ThunderConnector.command('stop');},10000);
   }
+  if (key.name == "f" && key.shift == true) {
+    fire4();
+  }
+
+  // if shift is pressed wile using F to fire, the launcher will fire all 4 missiles
+
   if (key && key.ctrl && key.name == 'c') {
     process.stdin.pause();
   }
